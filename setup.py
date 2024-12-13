6f5695f9f69d70cdf5a0e6f1a01c5283a6028067
afsuarezg <asuarezg@stanford.edu>
2024-12-13 11:42:53 -0800

    Bump version to 0.1.3 in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nielsen_data_cleaning',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'pandas',
