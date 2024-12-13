4b56e81521f4cc567e08f934106afc01569dc11a
afsuarezg <asuarezg@stanford.edu>
2024-12-13 08:13:25 -0800

    Add setup.py and LICENSE for Nielsen data cleaning package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='NielsenDataCleaning',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    author='Andrés Felipe Suárez G.',
+    author_email='asuarezg@stanford.edu',
+    description='A package for cleaning Nielsen data',
+    url='https://github.com/afsuarezg/Nielsen_data_cleaning',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
