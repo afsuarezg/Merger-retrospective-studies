0e0c4c59c747e1f3a37780bf19f5733ab9c3b151
afsuarezg <asuarezg@stanford.edu>
2024-12-13 11:37:27 -0800

    Remove commented-out movements_data assignment in main function for cleaner code

--- nielsen_data_cleaning/descarga_merge.py
+++ nielsen_data_cleaning/descarga_merge.py
@@ -173,10 +173,9 @@ def extra_attributes_file():
 
 
 def main():
-
     #os.chdir(f'/oak/stanford/groups/polinsky/Nielsen_data/Mergers/{DIRECTORY_NAME}/nielsen_extracts/RMS/{YEAR}/Movement_Files/{DEPARTMENT_CODE}_{YEAR}/')
 
-    movements_data = movements_file()
+    # movements_data = movements_file()
     stores_data = stores_file()
     products_data = products_file()
     extra_attributes_data = extra_attributes_file()
