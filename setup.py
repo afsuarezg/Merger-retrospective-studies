9314af96e438c8ea5d20cadfaf35b26c8b91c8b9
afsuarezg <asuarezg@stanford.edu>
2024-12-14 09:09:55 -0800

    Bump version to 0.1.5 in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nielsen_data_cleaning',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     py_modules=['main'],
     install_requires=[
