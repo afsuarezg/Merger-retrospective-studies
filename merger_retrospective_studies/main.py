469c15a66b3041af373bde184bd6393a04faea35
afsuarezg <asuarezg@stanford.edu>
2025-02-26 12:28:20 -0800

    Update run function parameters and enhance observed prices extraction with new data processing functions

--- merger_retrospective_studies/main.py
+++ merger_retrospective_studies/main.py
@@ -364,9 +364,9 @@ def compile_data(product_data: pd.DataFrame,
 
 
 def run():
-    first_week=6
-    num_weeks=2
-    threshold_identified_earnings = 0.5
+    first_week=7
+    num_weeks=8
+    threshold_identified_earnings = 0.7
     optimization_algorithm = 'l-bfgs-b'
     product_data = creating_product_data_rcl(main_dir='/oak/stanford/groups/polinsky/Mergers/Cigarettes',
                                      movements_path='/oak/stanford/groups/polinsky/Mergers/Cigarettes/Nielsen_data/2014/Movement_Files/4510_2014/7460_2014.tsv' ,
--- merger_retrospective_studies/predicción_vs_observado/observed_prices_extraction.py
+++ merger_retrospective_studies/predicción_vs_observado/observed_prices_extraction.py
@@ -1,21 +1,32 @@
+import pandas as pd 
 
 from ..nielsen_data_cleaning.product_data_creation import creating_product_data_for_comparison
 
 def list_retailers_with_predictions(path: str):
+    data = pd.read_csv(path)
+    retailers_list = set(data['store_code_uc'])
+    return retailers_list
+
+
+def dict_retailers_brands(path:str)-> dict:
+    data = pd.read_csv(path)
+    return data.groupby('')[''].unique.apply(list).to_dict()
+
+
+def filter_observed_by_predicted_data(path:str) -> pd.DataFrame:
     
     return 
-
-
 
 
 def main():
     #obtener los códigos de los retailers para los que se generaron las comparaciones 
 
 
-    #obtener  
+    #obtener las marcas por retailer 
 
 
     #crear la base de datos con toda la información
+
 
 
     #filtrar la baes de datos 
