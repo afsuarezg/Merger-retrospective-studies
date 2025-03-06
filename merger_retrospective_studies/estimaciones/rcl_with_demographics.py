895ca6f4b725b285aef7b8bfac5c856b14e10518
afsuarezg <asuarezg@stanford.edu>
2025-03-05 20:38:14 -0800

    Update formulations in rcl_with_demographics to include product_ids absorption and adjust intercept for nicotine model

--- merger_retrospective_studies/estimaciones/rcl_with_demographics.py
+++ merger_retrospective_studies/estimaciones/rcl_with_demographics.py
@@ -61,8 +61,8 @@ def rcl_with_demographics(product_data: pd.DataFrame, agent_data: pd.DataFrame):
 def rcl_with_demographics(product_data: pd.DataFrame, agent_data: pd.DataFrame):
 
     # Problem formulations without demographics
-    X1_formulation = pyblp.Formulation('0 + prices ')
-    X2_formulation = pyblp.Formulation('0 + nicotine + tar + co + nicotine_mg_per_g')
+    X1_formulation = pyblp.Formulation('0 + prices ', absorb='C(product_ids)')
+    X2_formulation = pyblp.Formulation('1 + nicotine + tar + co + nicotine_mg_per_g')
     product_formulations = (X1_formulation, X2_formulation)
 
     # Optimization algorithm
