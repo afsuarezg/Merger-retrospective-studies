d24ad7ecab8dd8f87573161d23b940f3441539c2
afsuarezg <asuarezg@stanford.edu>
2025-10-04 09:43:02 -0700

    Update requirements and setup for improved dependency management
    - Refactored `requirements.txt` to specify core and additional dependencies with version constraints for better compatibility.
    - Modified `setup.py` to read dependencies directly from `requirements.txt`, ensuring a streamlined installation process.
    - Updated Python version requirement in `setup.py` to restrict to versions between 3.7 and 3.12.

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read requirements from requirements.txt
+with open('requirements.txt', 'r') as f:
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
 setup(
     name='merger_retrospective_studies',
     version='0.1.8',
     packages=find_packages(include=['merger_retrospective_studies', 'merger_retrospective_studies.*']),
-    install_requires=[
-        'pandas',
-        'numpy',
-        'scipy',
-        'matplotlib',
-    ],
+    install_requires=requirements,
     author='Andrés Felipe Suárez G.',
     author_email='asuarezg@stanford.edu',
     description='A package for cleaning and processing Nielsen data',
@@ -20,5 +18,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7,<3.12',
 )
